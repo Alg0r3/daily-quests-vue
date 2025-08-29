90b151f4c988eb87d72d7a1bb18f274dfb19f560
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2025-08-30 01:29:33 +0200

    feat: move router to shared folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
+import router from './shared/router'
 
 const pinia = createPinia()
 const app = createApp(App)
