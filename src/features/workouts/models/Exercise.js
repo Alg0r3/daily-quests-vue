e57380358986b4decf49bbbc50617eb028e2848e
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2025-09-03 23:19:10 +0200

    feat(types): improve type definitions

--- src/features/workouts/models/Exercise.js
+++ src/features/workouts/models/Exercise.js
@@ -1,10 +1,12 @@
+/** @typedef {import('./ExerciseSet.js').ExerciseSet} ExerciseSet */
+
 /**
  * @typedef {object} Exercise
  * @property {string} id - Unique identifier (UUIDv7)
  * @property {string} workoutId - ID of the workout associated with the exercise
  * @property {string} movementId - ID of the movement associated with the exercise
- * @property {Set[]} sets - List of sets performed for the exercise
+ * @property {ExerciseSet[]} sets - List of sets performed for the exercise
  * @property {string} [notes] - Additional notes about the exercise (optional)
  */
 
-export {}
+export {};
--- src/features/workouts/models/ExerciseSet.js
+++ src/features/workouts/models/ExerciseSet.js
@@ -1,7 +1,7 @@
 /**
- * @typedef {object} Set
+ * @typedef {object} ExerciseSet
  * @property {string} id - Unique identifier (UUIDv7)
  * @property {number} reps - Number of reps
  */
 
-export {}
+export {};
