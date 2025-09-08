b35186ed3f3475b0f31b77175286b01e98e8e9ec
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2025-09-08 15:27:06 +0200

    feat(workouts): improve form
    - Improve accessibility
    - Add create/cancel events

--- src/features/workouts/components/WorkoutForm.vue
+++ src/features/workouts/components/WorkoutForm.vue
@@ -1,22 +1,41 @@
 <script setup lang="js">
 import { useWorkoutsStore } from '@/features/workouts/stores/workouts.store.js';
-import { ref } from 'vue';
+import { ref, onMounted, nextTick } from 'vue';
 
 const workoutsStore = useWorkoutsStore();
 const name = ref('');
 
+/** @type {import('vue').Ref<HTMLInputElement|null>} */
+const firstInput = ref(null);
+
+const emit = defineEmits(['created', 'cancelled']);
+
 async function onSubmit() {
   await workoutsStore.create({ name: name.value });
+
   name.value = '';
+  emit('created');
 }
+
+onMounted(async () => {
+  // Ensure DOM is updated before focusing on the first input
+  await nextTick();
+
+  const element = firstInput.value;
+
+  if (element) {
+    element.focus();
+  }
+});
 </script>
 
 <template>
   <h2>New workout</h2>
-  <form @submit.prevent="onSubmit">
+  <form @submit.prevent="onSubmit" @keyup.esc="emit('cancelled')">
     <label for="name">Name</label>
-    <input id="name" type="text" placeholder="Type a name" v-model.trim="name" />
-    <button type="submit">Add</button>
+    <input id="name" type="text" placeholder="Type a name" ref="firstInput" v-model.trim="name" />
+    <button type="submit">Create</button>
+    <button type="button" @click="emit('cancelled')">Cancel</button>
   </form>
 </template>
 
--- src/pages/WorkoutPage.vue
+++ src/pages/WorkoutPage.vue
@@ -5,21 +5,42 @@ import { ref } from 'vue';
 
 const isFormVisible = ref(false);
 
-function toggleFormVisibility() {
-  isFormVisible.value = !isFormVisible.value;
+/** @type {import('vue').Ref<HTMLButtonElement|null>} */
+const createButton = ref(null);
+
+function openForm() {
+  isFormVisible.value = true;
+}
+
+function closeForm() {
+  isFormVisible.value = false;
+
+  const element = createButton.value;
+
+  if (element) {
+    element.focus();
+  }
 }
 </script>
 
 <template>
   <h1>My Workouts</h1>
 
-  <Transition name="fade">
-    <WorkoutForm v-if="isFormVisible" />
-  </Transition>
-
-  <button @click="toggleFormVisibility">
-    {{ isFormVisible ? 'Cancel' : 'Add workout' }}
+  <button
+    type="button"
+    ref="createButton"
+    @click="openForm"
+    :aria-expanded="isFormVisible"
+    aria-controls="create-workout"
+  >
+    Create a workout
   </button>
+
+  <Transition name="fade">
+    <section id="create-workout" v-if="isFormVisible">
+      <WorkoutForm @created="closeForm" @cancelled="closeForm" />
+    </section>
+  </Transition>
 
   <WorkoutList />
 </template>
